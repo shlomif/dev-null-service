00f2d5bfc005f167f13d0991e6b12fa6c90354c7
Shlomi Fish <shlomif@shlomifish.org>
2019-08-08 21:58:10 +0300

    not working proto.

---
+++ dev-null-service/golang/main.go
@@ -0,0 +1,15 @@
+//
+// main.go
+// Copyright (C) 2019 Shlomi Fish <shlomif@cpan.org>
+//
+// Distributed under terms of the MIT license.
+//
+
+package main
+
+import (
+    "devnull/fcgi/child"
+)
+
+func main() {
+}

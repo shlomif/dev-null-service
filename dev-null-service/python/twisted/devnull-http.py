aa21ab0ab61662de4edfe4909bce92c146c043f5
Shlomi Fish <shlomif@shlomifish.org>
2020-08-04 12:10:22 +0300

    first py3 twisted prototype - curl gets stuck

---
+++ dev-null-service/python/twisted/devnull-http.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2020 Shlomi Fish <shlomif@cpan.org>
+#
+# Distributed under the MIT license.
+
+"""
+devnull HTTP
+"""
+from twisted.internet import endpoints, protocol, reactor
+
+
+class Echo(protocol.Protocol):
+    BEFORE_HEADER = 0
+    AFTER_HEADER = 1
+    HEADER_ERROR = 2
+    MAX_HEADER_LEN = 1024
+
+    def __init__(self):
+        super(Echo, self).__init__()
+        self.state = self.BEFORE_HEADER
+        self.header = bytes()
+
+    def dataReceived(self, data):  # noqa: N802
+        if self.state == self.BEFORE_HEADER:
+            self.header += data
+            if "\r\n\r\n".encode('utf-8') in self.header:
+                self.state = self.AFTER_HEADER
+            elif len(self.header) > self.MAX_HEADER_LEN:
+                self.state = self.HEADER_ERROR
+
+
+class EchoFactory(protocol.Factory):
+    def buildProtocol(self, addr):  # noqa: N802
+        return Echo()
+
+
+endpoints.serverFromString(reactor, "tcp:1234").listen(EchoFactory())
+reactor.run()
